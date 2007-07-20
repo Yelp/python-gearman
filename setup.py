4e6ba823cc75336c0157d5471f5d96b5c77e16d8
Samuel Stauffer <samuel@descolada.com>
2007-07-20 19:16:24 +0000

    Removed "author" lines from python api setup.py - too limited, only allows one author
    git-svn-id: http://code.sixapart.com/svn/gearman/trunk/api/python/lib@309 011c6a6d-750f-0410-a5f6-93fdcd050bc4

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from distutils.core import setup
 setup(name='pygear',
       version='0.1',
       description='Gearman Client',
-      author='Samuel Stauffer',
-      author_email='samuel@descolada.com',
       url='http://danga.com/gearman/',
       packages=['gearman'],
      )
