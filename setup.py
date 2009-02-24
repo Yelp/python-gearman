42daddce3fcb592adf1ed990cda0a75e022251c8
Samuel Stauffer <samuel@descolada.com>
2009-02-24 01:14:49 +0000

    pythonapi: Updated setup.py to include necessary info for PyPI
    git-svn-id: http://code.sixapart.com/svn/gearman/trunk/api/python/lib@401 011c6a6d-750f-0410-a5f6-93fdcd050bc4

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='pygear',
-      version='0.1',
-      description='Gearman Client',
-      url='http://danga.com/gearman/',
-      packages=['gearman'],
-     )
+setup(
+    name = 'gearman',
+    version = '1.0',
+    description = 'Gearman Client',
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@descolada.com',
+    url = 'http://danga.com/gearman/',
+    packages = ['gearman'],
+)
