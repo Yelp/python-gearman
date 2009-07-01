7a5eba1378222cd36863f3a26432609dc731e3ad
Samuel Stauffer <samuel@descolada.com>
2009-07-01 13:05:10 -0700

    Version 1.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'gearman',
-    version = '1.3.0',
+    version = '1.3.1',
     description = 'Gearman Library',
     author = 'Samuel Stauffer',
     author_email = 'samuel@descolada.com',
