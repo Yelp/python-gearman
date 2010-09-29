374f0d6877d200dc363d1857bb74d2803b2bc040
Matthew Tai <mtai@yelp.com>
2010-09-29 15:53:31 -0700

    Authorship bump on the docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-gearman'
-copyright = u'2010, Matthew Tai, Eskil Olsen'
+copyright = u'2010, Matthew Tai'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
