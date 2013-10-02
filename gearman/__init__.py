7030a1ec560c63b8c9238cda59aea182fb1b0e49
Kevin Morey <kevin.morey@gmail.com>
2013-10-01 21:52:02 -0500

    Revert "Gearman tweaks to fix bug"
    This reverts commit 2ea5ca7f145c6bd7a083305abb98bdc802733479.

--- gearman/__init__.py
+++ gearman/__init__.py
@@ -2,7 +2,7 @@
 Gearman API - Client, worker, and admin client interfaces
 """
 
-__version__ = '2.1.0-lctv'
+__version__ = '2.0.2'
 
 from gearman.admin_client import GearmanAdminClient
 from gearman.client import GearmanClient
