46c0832fd8a7260848e2b1ba0018f33ed206d747
Pai-Wei Lai <paiwei@yelp.com>
2015-03-13 15:38:45 -0700

    bump version to v2.0.3

--- gearman/__init__.py
+++ gearman/__init__.py
@@ -2,7 +2,7 @@
 Gearman API - Client, worker, and admin client interfaces
 """
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 from gearman.admin_client import GearmanAdminClient
 from gearman.client import GearmanClient
